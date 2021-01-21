ed4eff9c32c3b1be589492747644277a929cf97c
pa4ul <paulherbich1@gmail.com>
2021-01-21 15:30:59 +0100

    design

--- src/components/impressum.vue
+++ src/components/impressum.vue
@@ -63,6 +63,7 @@
 
     <p>
       <a href="https://www.anthonyboyd.graphics/?utm_source=file_shortcuts&utm_medium=downloaded_files"> Mockup von Macbook </a>
+      <br />
       <a href="https://www.oursky.com/?utm_medium=web&utm_source=mockuphone"> Mockup von Iphone </a>
     </p>
     
